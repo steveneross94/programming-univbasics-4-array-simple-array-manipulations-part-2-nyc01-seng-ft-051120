97c0ede0caec2a58ab2ceec5dfb3fe6bc6bb552b
Steven Ross <ross.steven@me.com>
2020-04-10 23:43:06 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,9 +3,7 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array = [" ", " ", " ", " ", " ", " ", " "]
-  element = "Python"
-  new_array = array.insert(4,element)
+  array.insert(4, element)
 end
 
 def using_uniq(array)
@@ -13,17 +11,13 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
-  instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
-  flat_array = instruments.flatten
+  array.flatten
 end
 
 def using_delete(array, string)
-  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = instructors.delete("Steven")
-  no_offense_steven
+  array.delete(string)
 end
 
 def using_delete_at(array, integer)
-  famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-  deleted_robot = famous_robots.delete_at(2)
+  array.delete_at(2)
 end
