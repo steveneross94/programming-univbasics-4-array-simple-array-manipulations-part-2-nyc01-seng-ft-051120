e9cf4e67ce19210a0df6583afffa5e0d3b2f28d7
Steven Ross <ross.steven@me.com>
2020-04-10 23:42:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,11 +11,11 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
-  arra.flatten
+  array.flatten
 end
 
 def using_delete(array, string)
-  array.delete(string)  
+  array.delete(string)
 end
 
 def using_delete_at(array, integer)
